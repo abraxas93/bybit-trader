c00b64187ca5f17b46bfdbc9304a19d53038bcbb
Savchuk Alexandr <savchyk.a@gmail.com>
2023-11-25 14:04:39 +0530

    feat: add store reset script

---
+++ src/scripts/store.ts
@@ -0,0 +1,24 @@
+import {RKEYS} from '../constants';
+import {Redis} from 'ioredis';
+
+export async function setupTradeOptions() {
+  try {
+    const redis = new Redis();
+
+    await redis.set(RKEYS.POS_QTY, '0');
+    await redis.set(RKEYS.LAST_AVG_ORD_PRICE, '0');
+
+    await redis.set(RKEYS.AVG_ORDER_EXISTS, 'false');
+    await redis.set(RKEYS.POSITION_OPENED, 'false');
+    await redis.set(RKEYS.AVG_POS_PRICE, '0');
+    await redis.set(RKEYS.AVG_ORDER_COUNT, '0');
+    await redis.set(RKEYS.PROFIT_TAKES_COUNT, 0);
+  } catch (error) {
+    console.error(JSON.stringify(error));
+  }
+}
+
+(async () => {
+  await setupTradeOptions();
+  console.log('>>> dev:reset:store');
+})().catch(err => console.error(err));
