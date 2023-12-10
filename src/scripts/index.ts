9ce236a612fe54c7517d22764d7cdd05752abfa7
Savchuk Alexandr <savchyk.a@gmail.com>
2023-12-10 23:25:11 +0530

    update setup script

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -14,32 +14,39 @@ import {RKEYS} from '../constants';
 import {Redis} from 'ioredis';
 
 export async function setupTradeOptions() {
+  const symbol = process.argv[2];
+  const userId = process.argv[3];
+  const env = process.env.NODE_ENV || 'dev';
+  if (!symbol) throw new Error(`Symbol required`);
+  if (!userId) throw new Error(`UserId required`);
   try {
     const redis = new Redis();
-    await redis.set(RKEYS.SYMBOL, SYMBOL);
-    await redis.set(RKEYS.QUANTITY, BASE_QUANTITY);
-    await redis.set(RKEYS.PERIOD, TIME_FRAME);
-    await redis.set(RKEYS.MARTIN_GALE, MARTIN_GALE);
+    const baseKey = `${userId}:${env}:${symbol}`;
 
-    await redis.set(RKEYS.PROFIT_RATE, TAKE_PROFIT_RATE);
-    await redis.set(RKEYS.AVG_RATE, AVG_BUY_RATE);
+    await redis.set(`${baseKey}:${RKEYS.SYMBOL}`, symbol);
+    await redis.set(`${baseKey}:${RKEYS.QUANTITY}`, BASE_QUANTITY);
+    await redis.set(`${baseKey}:${RKEYS.PERIOD}`, TIME_FRAME);
+    await redis.set(`${baseKey}:${RKEYS.MARTIN_GALE}`, MARTIN_GALE);
 
-    await redis.set(RKEYS.MAX_AVG_COUNT, MAX_AVG_ORDER_COUNT);
-    await redis.set(RKEYS.MIN_CANDLES, CANDLES_TO_WAIT);
+    await redis.set(`${baseKey}:${RKEYS.PROFIT_RATE}`, TAKE_PROFIT_RATE);
+    await redis.set(`${baseKey}:${RKEYS.AVG_RATE}`, AVG_BUY_RATE);
 
-    await redis.set(RKEYS.DIGITS, DIGITS_AFTER_COMMA);
-    await redis.set(RKEYS.MARTINGALE, MARTIN_GALE);
-    await redis.set(RKEYS.CATEGORY, 'linear');
-    await redis.set(RKEYS.TRADE_CYCLES, TRADE_CYCLES);
+    await redis.set(`${baseKey}:${RKEYS.MAX_AVG_COUNT}`, MAX_AVG_ORDER_COUNT);
+    await redis.set(`${baseKey}:${RKEYS.MIN_CANDLES}`, CANDLES_TO_WAIT);
 
-    await redis.set(RKEYS.POS_QTY, '0');
-    await redis.set(RKEYS.LAST_AVG_ORD_PRICE, '0');
+    await redis.set(`${baseKey}:${RKEYS.DIGITS}`, DIGITS_AFTER_COMMA);
+    await redis.set(`${baseKey}:${RKEYS.MARTINGALE}`, MARTIN_GALE);
+    await redis.set(`${baseKey}:${RKEYS.CATEGORY}`, 'linear');
+    await redis.set(`${baseKey}:${RKEYS.TRADE_CYCLES}`, TRADE_CYCLES);
 
-    await redis.set(RKEYS.AVG_ORDER_EXISTS, 'false');
-    await redis.set(RKEYS.POSITION_OPENED, 'false');
-    await redis.set(RKEYS.AVG_POS_PRICE, '0');
-    await redis.set(RKEYS.AVG_ORDER_COUNT, '0');
-    await redis.set(RKEYS.PROFIT_TAKES_COUNT, 0);
+    await redis.set(`${baseKey}:${RKEYS.POS_QTY}`, '0');
+    await redis.set(`${baseKey}:${RKEYS.LAST_AVG_ORD_PRICE}`, '0');
+
+    await redis.set(`${baseKey}:${RKEYS.AVG_ORDER_EXISTS}`, 'false');
+    await redis.set(`${baseKey}:${RKEYS.POSITION_OPENED}`, 'false');
+    await redis.set(`${baseKey}:${RKEYS.AVG_POS_PRICE}`, '0');
+    await redis.set(`${baseKey}:${RKEYS.AVG_ORDER_COUNT}`, '0');
+    await redis.set(`${baseKey}:${RKEYS.PROFIT_TAKES_COUNT}`, 0);
   } catch (error) {
     console.error(JSON.stringify(error));
   }
